9f1065c0584c1db52f6a3512ee36ca6b3c1b6889
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-05-19 12:55:17 +0200

    fix: apply lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,8 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/restrict-template-expressions': 'off'
   }
 }
