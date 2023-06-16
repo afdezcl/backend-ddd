c3b5fda4285fdf8bcd03abf98f79fada399d92f8
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-06-16 13:09:32 +0200

    fix: linter and apply new rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,13 @@ module.exports = {
     project: './tsconfig.json'
   },
   rules: {
-    '@typescript-eslint/restrict-template-expressions': 'off'
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    'n/no-path-concat': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
   }
 }
