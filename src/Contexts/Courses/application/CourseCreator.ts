41a756655b07520ce28135d7751aee45f6505172
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-06-23 12:38:41 +0200

    refactor: create interface to create course

--- src/Contexts/Courses/application/CourseCreator.ts
+++ src/Contexts/Courses/application/CourseCreator.ts
@@ -3,13 +3,17 @@ import { CourseDuration } from '../domain/CourseDuration'
 import { CourseId } from '../domain/CourseId'
 import { CourseName } from '../domain/CourseName'
 import { CourseRepository } from '../domain/CourseRepository'
+import { CreateCourseRequest } from './CreateCourseRequest'
 
 export class CourseCreator {
   constructor(private readonly courseRepository: CourseRepository) { }
 
-  async run(id: CourseId, duration: CourseDuration, name: CourseName) {
-    const course = new Course(id, duration, name)
-
+  async run(request: CreateCourseRequest): Promise<void> {
+    const course = new Course(
+      new CourseId(request.id),
+      new CourseDuration(request.duration),
+      new CourseName(request.name)
+    )
     await this.courseRepository.save(course)
   }
 }
