2b1a67194019569a313ac173d8c54d49f6e46405
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-07-09 18:24:59 +0200

    feat: connect typeorm repository

--- src/apps/platform/controllers/CoursesPostController.ts
+++ src/apps/platform/controllers/CoursesPostController.ts
@@ -7,14 +7,14 @@ export class CoursesPostController implements Controller {
   constructor(private readonly courseCreator: CourseCreator) { }
 
   async run(req: Request, res: Response) {
-    const id: string = req.params.id
+    const id: string = req.body.id
     const name: string = req.body.name
     const duration: string = req.body.duration
 
     try {
       await this.courseCreator.run({ id, name, duration })
     } catch (error: any) {
-      res.status(httpStatus.BAD_REQUEST).send(error.message)
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
     }
 
     res.status(httpStatus.CREATED).send()
