a74d7ded9c7bb0737d23b75a4ffd931bd4cef6bf
Adrian Fernandez <adrian.fernandez@zinklar.com>
2023-07-23 16:56:56 +0200

    refactor: update script sql

--- database/init/script.sql
+++ database/init/script.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE courses;
-
 CREATE TABLE IF NOT EXISTS courses (
   id varchar(255),
   name varchar(255),
   duration varchar(255)
 );
+
+INSERT INTO courses (id, name, duration) VALUES
+('1', 'Java', '3 months');
